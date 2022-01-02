d10fded3882af6c39e92037337e6bb14c2a8257f
Meet <meetmakwana203.@gmail.com>
2022-01-03 01:08:41 +0530

    3rd draft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,14 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Navbar() {
+
+    const navFunc = () =>{
+        console.log("Here");
+    }
     return (
         <>
             <div className='header' style={{ backgroundColor: "#121921", height: "60px", color: "white" }}>
@@ -26,34 +32,35 @@ function Navbar() {
 
                 <div className="nav-mid">
                     <div className="dropdown">
-                        <button className="btn btn-secondary dropdown-toggle dropdown-all" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <button className="btn btn-secondary dropdown-toggle dropdown-all text-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             All
                         </button>
                         <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a className="dropdown-item" href="#">Alexa Skills</a></li>
-                            <li><a className="dropdown-item" href="#">Amazon Fashion</a></li>
-                            <li><a className="dropdown-item" href="#">Amazon Fresh</a></li>
-                            <li><a className="dropdown-item" href="#">Appliances</a></li>
-                            <li><a className="dropdown-item" href="#">Apps & Games</a></li>
-                            <li><a className="dropdown-item" href="#">Baby</a></li>
-                            <li><a className="dropdown-item" href="#">Beauty</a></li>
-                            <li><a className="dropdown-item" href="#">Books</a></li>
+                            <li><a className="dropdown-item" href="/">Alexa Skills</a></li>
+                            <li><a className="dropdown-item" href="/">Amazon Fashion</a></li>
+                            <li><a className="dropdown-item" href="/">Amazon Fresh</a></li>
+                            <li><a className="dropdown-item" href="/">Appliances</a></li>
+                            <li><a className="dropdown-item" href="/">Apps & Games</a></li>
+                            <li><a className="dropdown-item" href="/">Baby</a></li>
+                            <li><a className="dropdown-item" href="/">Beauty</a></li>
+                            <li><a className="dropdown-item" href="/">Books</a></li>
                         </ul>
                     </div>
-                    <input className='nav-search' type="text" />
+                    <input onClick={navFunc()} className='nav-search' type="text" />
                     <SearchIcon type="button" className='searchIcon' style={{ fill: "black" }} />
                 </div>
 
                 <div className="nav-right">
                     <div className="country" type="button">
                         <img src="https://i.ibb.co/wWfcTbB/in-square-01.png" alt="" />
+                        <ArrowDropDownOutlinedIcon className='country-arrow'/>
                     </div>
-                    <div className="lang"></div>
-                    <div className="nav-account">
+                    <div className="nav-account" type="button">
                         <span className='span-h2'>Hello, sign in </span>
                         <span className='span-h1'>Accounts & lists</span>
+                        <ArrowDropDownOutlinedIcon className='account-arrow'/>
                     </div>
-                    <div className="nav-orders">
+                    <div className="nav-orders" type="button">
                         <span className='span-h2'>Returns</span>
                         <span className='span-h1'>& Orders</span>
                     </div>
@@ -67,23 +74,23 @@ function Navbar() {
 
             <div className="header-bottom">
                 <ul>
-                    <li>All</li>
-                    <li>Gift Cards</li>
-                    <li>Best Sellers</li>
-                    <li>Mobiles</li>
-                    <li>Customer Care</li>
-                    <li>Electronics</li>
-                    <li>Today's Deals</li>
-                    <li>Fashion</li>
-                    <li>Prime</li>
-                    <li>Home and Kitchen</li>
-                    <li>Amazon Pay</li>
-                    <li>New Releases</li>
-                    <li>Computers</li>
-                    <li>Books</li>
-                    <li>Coupons</li>
-                    <li>Toys and Games</li>
-                    <li>Sell</li>
+                    <li type="button"><MenuIcon className='menu-icon'/> All</li>
+                    <li type="button">Gift Cards</li>
+                    <li type="button">Best Sellers</li>
+                    <li type="button">Mobiles</li>
+                    <li type="button">Customer Care</li>
+                    <li type="button">Electronics</li>
+                    <li type="button">Today's Deals</li>
+                    <li type="button">Fashion</li>
+                    <li type="button">Prime</li>
+                    <li type="button">Home and Kitchen</li>
+                    <li type="button">Amazon Pay</li>
+                    <li type="button">New Releases</li>
+                    <li type="button">Computers</li>
+                    <li type="button">Books</li>
+                    <li type="button">Coupons</li>
+                    <li type="button">Toys and Games</li>
+                    <li type="button">Sell</li>
                 </ul>
             </div>
         </>
